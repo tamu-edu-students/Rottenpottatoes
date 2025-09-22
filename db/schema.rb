c9b6a01388deb22a036bda5230d8a516a7ad1706
VenkateswarluNagineni <venkates2002@tamu.edu>
2025-09-22 10:49:06 -0500

    Add full CRUD, seeds, sortable table, and CSS styling

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_22_152302) do
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "rating"
+    t.text "description"
+    t.date "release_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Movie.create([
+  {title: "Inception", rating: "PG-13", description: "Dream heist.", release_date: "2010-07-16"},
+  {title: "The Matrix", rating: "R", description: "Virtual reality.", release_date: "1999-03-31"},
+  {title: "Finding Nemo", rating: "G", description: "Fish adventure.", release_date: "2003-05-30"},
+  {title: "Interstellar", rating: "PG-13", description: "Space exploration.", release_date: "2014-11-07"},
+  {title: "The Godfather", rating: "R", description: "Crime family saga.", release_date: "1972-03-24"},
+  {title: "The Lion King", rating: "G", description: "Lion cub's journey.", release_date: "1994-06-24"}
+])
